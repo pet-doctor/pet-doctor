4552792b8e2bb835433ed9fee858088ad3114252
Alex <s.bosov2012@yandex.ru>
2022-10-19 14:20:28 +0300

    Initial commit

---
+++ src/main/java/com/petdoctor/PetDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.petdoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDoctorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetDoctorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petdoctor/PetDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petdoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
