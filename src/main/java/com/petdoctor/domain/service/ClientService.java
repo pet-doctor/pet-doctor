e87a820f387afa36910e6f6b37cd45c36c607bf2
Alex <s.bosov2012@yandex.ru>
2022-10-31 04:35:35 +0300

    feat: add empty methods for ClientService

--- src/main/java/com/petdoctor/domain/service/ClientService.java
+++ src/main/java/com/petdoctor/domain/service/ClientService.java
@@ -1,4 +1,18 @@
 package com.petdoctor.domain.service;
 
+import com.petdoctor.domain.dto.AppointmentDto;
+import com.petdoctor.domain.dto.ClientDto;
+
+import java.util.List;
+
 public interface ClientService {
+    List<ClientDto> findAllClient();
+
+    ClientDto getClientById(Long id);
+
+    ClientDto saveClient(ClientDto clientDto);
+
+    ClientDto updateClient(ClientDto clientDto);
+
+    void deleteClientById(Long id);
 }
--- src/main/java/com/petdoctor/domain/service/impl/ClientServiceImpl.java
+++ src/main/java/com/petdoctor/domain/service/impl/ClientServiceImpl.java
@@ -1,6 +1,34 @@
 package com.petdoctor.domain.service.impl;
 
+import com.petdoctor.domain.dto.ClientDto;
 import com.petdoctor.domain.service.ClientService;
 
+import java.util.List;
+
 public class ClientServiceImpl implements ClientService {
+
+    @Override
+    public List<ClientDto> findAllClient() {
+        return null;
+    }
+
+    @Override
+    public ClientDto getClientById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ClientDto saveClient(ClientDto clientDto) {
+        return null;
+    }
+
+    @Override
+    public ClientDto updateClient(ClientDto clientDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteClientById(Long id) {
+
+    }
 }
