9fe95a508048328f88b7bd572808e8c64fb81572
Alex <s.bosov2012@yandex.ru>
2022-11-01 13:37:59 +0300

    feat: create VetClinicDto

--- src/main/java/com/petdoctor/domain/dto/VetClinicDto.java
+++ src/main/java/com/petdoctor/domain/dto/VetClinicDto.java
@@ -1,4 +1,40 @@
 package com.petdoctor.domain.dto;
 
+import java.util.List;
+
 public class VetClinicDto {
+    private Long id;
+    private String address;
+    private String email;
+    private List<DoctorDto> doctors;
+    private List<ClientDto> clients;
+
+
+    public VetClinicDto(Long id, String address, String email, List<DoctorDto> doctors, List<ClientDto> clients) {
+        this.id = id;
+        this.address = address;
+        this.email = email;
+        this.doctors = doctors;
+        this.clients = clients;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<DoctorDto> getDoctors() {
+        return doctors;
+    }
+
+    public List<ClientDto> getClients() {
+        return clients;
+    }
 }
