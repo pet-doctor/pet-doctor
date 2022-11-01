c1ddcefac9655675aa8d2ae31ff7af898dc1fff1
Alex <s.bosov2012@yandex.ru>
2022-11-01 14:01:40 +0300

    feat: create appointmentLiteDto for input

---
+++ src/main/java/com/petdoctor/domain/dto/lite/AppointmentLiteDto.java
@@ -0,0 +1,32 @@
+package com.petdoctor.domain.dto.lite;
+
+import com.petdoctor.domain.model.appointment.AppointmentState;
+
+import java.time.LocalDate;
+
+public class AppointmentLiteDto {
+    final private Long id;
+    final private LocalDate startTime;
+    final private AppointmentState appointmentState;
+
+    public AppointmentLiteDto(Long id,
+                              LocalDate startTime,
+                              AppointmentState appointmentState) {
+        this.id = id;
+        this.startTime = startTime;
+        this.appointmentState = appointmentState;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getStartTime() {
+        return startTime;
+    }
+
+    public AppointmentState getAppointmentState() {
+        return appointmentState;
+    }
+}
