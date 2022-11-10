c2c95334abae643def838dbd77e90e4f2b9eb79f
feelinLit <podshivalov.o29@gmail.com>
2022-11-10 18:32:09 +0300

    chore: add javadoc
    Signed-off-by: feelinLit <podshivalov.o29@gmail.com>

--- domain/src/main/java/com/petdoctor/domain/service/VetClinicService.java
+++ domain/src/main/java/com/petdoctor/domain/service/VetClinicService.java
@@ -3,11 +3,26 @@ package com.petdoctor.domain.service;
 import com.petdoctor.domain.dto.VetClinicDto;
 
 public interface VetClinicService {
+    /**
+     * @param id VetClinic ID
+     * @return Immutable DTO of VetClinic
+     */
     VetClinicDto getVetClinicById(Long id);
 
+    /**
+     * @param vetClinicDto Immutable DTO of VetClinic
+     * @return Saved to Database DTO of VetClinic
+     */
     VetClinicDto saveVetClinic(VetClinicDto vetClinicDto);
 
+    /**
+     * @param vetClinicDto Immutable DTO of VetClinic
+     * @return Updated to Database DTO of VetClinic
+     */
     VetClinicDto updateVetClinic(VetClinicDto vetClinicDto);
 
+    /**
+     * @param id VetClinic ID
+     */
     void deleteVetClinicById(Long id);
 }
