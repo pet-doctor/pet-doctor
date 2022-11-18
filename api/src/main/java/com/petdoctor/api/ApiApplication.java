2ed3641d2b48c2c855a2376b8083deeba719502b
Alex <s.bosov2012@yandex.ru>
2022-11-18 17:37:50 +0300

    fix: do server ports different and fix bug with appointment beans

--- api/src/main/java/com/petdoctor/api/ApiApplication.java
+++ api/src/main/java/com/petdoctor/api/ApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = {"com.petdoctor.data.repository", "com.petdoctor.domain.service"}, exclude = JpaRepositoriesAutoConfiguration.class)
+@SpringBootApplication(scanBasePackages = {"com.petdoctor.data.repository"}, exclude = JpaRepositoriesAutoConfiguration.class)
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
