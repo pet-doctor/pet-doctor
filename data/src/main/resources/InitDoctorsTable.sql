4a1416ebce5abbd86bc84641d7da8b25e73881b1
Gvozdoslav <remrostrus2123@yandex.ru>
2022-11-21 17:15:07 +0300

    feat: add db startup entities
    Signed-off-by: Gvozdoslav <remrostrus2123@yandex.ru>

---
+++ data/src/main/resources/InitDoctorsTable.sql
@@ -0,0 +1,2 @@
+insert into pet.public.doctor_entity (id, doctor_office, email, name, surname, vet_clinic_entity_id)
+values (1, '123', 'doctor@mapper.ru', 'mapper', 'osas', 1);
---
+++ data/src/main/resources/InitVetClinicsTable.sql
@@ -0,0 +1,2 @@
+insert into pet.public.vet_clinic_entity (id, address, email)
+values (1, 'sovetskaya-14-12-14-12', 'vet@koshka.ru');
